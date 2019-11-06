f945d423557ae23e56d19b37fd7c7627eb3564b0
xiangbo <1763695899@qq.com>
2019-11-06 23:08:24 +0800

    初始建立项目

---
+++ src/Infrastructure/Identity/AppDbContextSeed.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Identity
+{
+    public static class AppDbContextSeed
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager)
+        {
+            var defaultUser = new ApplicationUser()
+            {
+                UserName = "test",
+                Email = "test@test.com"
+            };
+            await userManager.CreateAsync(defaultUser, "test");
+        }
+    }
+}
---
+++ src/Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        /// <summary>
+        /// 推荐码
+        /// </summary>
+        public string RegistCode { get; set; }
+    }
+}
